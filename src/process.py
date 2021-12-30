34c7e41d0536c8005eb471886e85464fc61583cb
charlyalizadeh <charlypro1502@outlook.fr>
2021-12-30 17:32:37 +0100

    Add vectorization and categorization of features

--- src/process.py
+++ src/process.py
@@ -1,37 +1,25 @@
+from embedder import Embedder
 import pandas as pd
-import torch
-import spacy
-from transformers import FlaubertModel, FlaubertTokenizer
 
-#import fasttext.util
-#fasttext.util.download_model('fr', if_exists='ignore')
-#ft = fasttext.load_model('cc.fr.300.bin')
 
-#train = pd.read_csv('../data/clean/train.csv', sep=';')
-#test = pd.read_csv('../data/clean/test.csv', sep=';')
-#nlp = spacy.load("fr_core_news_sm")
-modelname = 'flaubert/flaubert_base_cased'
-#
-#flaubert, log = FlaubertModel.from_pretrained(modelname, output_loading_info=True)
-flaubert_tokenizer = FlaubertTokenizer.from_pretrained(modelname, do_lowercase=False)
-#
-#def process_df(df):
-#    # Drop useless columns
-#    df.drop(['auteur'], axis=1, inplace=True)
-#
-#    # Convert categorical features to numerical
-#    for c in ['assureur', 'produit']:
-#        df[c] = df[c].astype('category')
-#        df[c] = df[c].cat.codes
-#
-#
-sentence = "Salut ça va ?"
-sentence_2 = "salut ça va ? Oui et toi ?"
-token_ids = torch.tensor([flaubert_tokenizer.encode(sentence)])
-token_ids_2 = torch.tensor([flaubert_tokenizer.encode(sentence_2)])
-print(token_ids)
-print(token_ids.shape)
-print(token_ids_2)
-print(token_ids_2.shape)
-#last_layer = flaubert(token_ids)[0]
-#print(last_layer.shape)
+embedder = Embedder('spacy')
+
+
+def process_df(df):
+    df['avis'] = df['avis'].apply(lambda s: ','.join(map(str, embedder[s])))
+    for c in ['assureur', 'produit']:
+        df[c] = df[c].astype('category')
+        df[c] = df[c].cat.codes
+    df['dayofweek'] = df['date'].dt.dayofweek
+    df['day'] = df['date'].dt.day
+    df['month'] = df['date'].dt.month
+    df['year'] = df['date'].dt.year
+    df.drop(['auteur', 'date'], axis=1, inplace=True, errors='ignore')
+
+
+train_df = pd.read_csv('./data/clean/train.csv', sep=';', parse_dates=['date'])
+test_df = pd.read_csv('./data/clean/test.csv', sep=';', parse_dates=['date'])
+process_df(train_df)
+process_df(test_df)
+train_df.to_csv('./data/processed/train.csv', sep=';', index=False)
+test_df.to_csv('./data/processed/test.csv', sep=';', index=False)
--- src/vectorize.py
+++
@@ -1,8 +0,0 @@
-
-# French pre trained word embedding
-## * https://fasttext.cc/docs/en/crawl-vectors.html
-## * https://github.com/Ismailhachimi/French-Word-Embeddings
-
-# Or tokenizer
-## FlauBERT
-## RoBERT
