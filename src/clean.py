41fca2a2a9ad63c1fd69495b7d9b9cd2a7123ec9
charlyalizadeh <charlypro1502@outlook.fr>
2021-12-30 17:30:42 +0100

    Remove rows with a NaN `avis` and modify data path

--- src/clean.py
+++ src/clean.py
@@ -16,19 +16,20 @@ month_dict = {
         'novembre': '11',
         'décembre': '12'
 }
-os.makedirs('../data/clean', exist_ok=True)
+os.makedirs('./data/clean', exist_ok=True)
 
 
 def clean_df(df):
     for k, v in month_dict.items():
-        df['date'] = train_df['date'].str.replace(k, v)
+        df['date'] = df['date'].str.replace(k, v)
     df['date'] = df['date'].apply(lambda s: s.strip()[:10])
     df['date'] = pd.to_datetime(df['date'], format='%d %m %Y')
+    df.dropna(subset=['avis'], inplace=True)
 
 
-train_df = pd.read_csv('../data/raw/train.csv', sep=';')
-test_df = pd.read_csv('../data/raw/test.csv', sep=';')
+train_df = pd.read_csv('./data/raw/train.csv', sep=';')
+test_df = pd.read_csv('./data/raw/test.csv', sep=';')
 clean_df(train_df)
 clean_df(test_df)
-train_df.to_csv('../data/clean/train.csv', sep=';', index=False)
-test_df.to_csv('../data/clean/test.csv', sep=';', index=False)
+train_df.to_csv('./data/clean/train.csv', sep=';', index=False)
+test_df.to_csv('./data/clean/test.csv', sep=';', index=False)
