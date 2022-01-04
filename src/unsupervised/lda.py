059dc37469ce4d6a43503ef81c92cd3c861e4897
charlyalizadeh <charlypro1502@outlook.fr>
2022-01-04 13:19:54 +0100

    Show results in markdown format

--- src/unsupervised/lda.py
+++ src/unsupervised/lda.py
@@ -16,13 +16,16 @@ def lda_all(num_topics=1):
         passes=20,
         workers=4
     )
+    results = {'Topic': [], 'Words': []}
     for i in range(num_topics):
-        print(f"Topics {i}")
-        print(f"  Words: {[t[0] for t in lda_model.show_topic(i)]}")
+        results['Topic'].append(i)
+        results['Words'].append([t[0] for t in lda_model.show_topic(i)])
+    print(pd.DataFrame(data=results).to_markdown())
 
 
-def lda_split(num_topics_per_split=1):
+def lda_split():
     train_df = pd.read_csv('./data/processed/train_unsupervised.csv', sep=';', parse_dates=['date'])
+    results = {'Note': [], 'Words': []}
     for i in range(5):
         dictionary = gensim.corpora.Dictionary.load(f'./data/processed/dictionary_split_{i}')
         docs = train_df[train_df['note'] == i]['avis'].str.split()
@@ -30,12 +33,15 @@ def lda_split(num_topics_per_split=1):
 
         lda_model = LdaMulticore(
             bow_corpus,
-            num_topics=num_topics_per_split,
+            num_topics=1,
             id2word=dictionary,
             passes=20,
             workers=4
         )
-        print(f"Note: {i}")
-        for j in range(num_topics_per_split):
-            print(f"  Topics {j}")
-            print(f"    Words: {[t[0] for t in lda_model.show_topic(j)]}")
+        results['Note'].append(i)
+        results['Words'].append([t[0] for t in lda_model.show_topic(0)])
+    print(pd.DataFrame(data=results).to_markdown())
+
+
+lda_all()
+lda_split()
